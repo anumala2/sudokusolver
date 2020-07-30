f2384b62bc02e6c15ab64bf8474bc7e9aac4c1f8
Aadithya Anumala <aadianumala@Aadithyas-MacBook-Pro.local>
2020-07-29 19:00:10 -0700

    added functions to verify if sudoku number is valid and to create a puzzle

--- sudokusolver/puzzlecreator.py
+++ sudokusolver/puzzlecreator.py
@@ -1,6 +1,39 @@
 import numpy as np
 import pandas as pd
-import math
+import random
 
-SIZE = 6
-#for now let's set size as 6x6 sudoku puzzles
+def valid_insert(num, board, i, j):
+    if i==0:
+        if j==0:
+            return True
+    for col in range(0, j):
+        if board[i][col] == num:
+            return False
+    for row in range(0, i):
+        if board[row][j] == num:
+            return False
+    #print("passmain")
+    startbox = [int(i/3)*3, int(j/3)*3]
+    print(startbox)
+    for row in range(startbox[0], startbox[0]+3):
+        for col in range(startbox[1], startbox[1]+3):
+            if board[row][col] == num:
+                return False
+    return True
+
+#build a finished puzzle as a numpy 2d array
+def create_finished_puzzle(SIZE):
+    board = np.zeros((SIZE,SIZE), dtype=int)
+    random.seed(1)
+    for i in range(0, SIZE):
+        for j in range(0, SIZE):
+            filled = False
+            while not filled:
+                temp = random.randint(1, SIZE)
+                print(temp, i, j)
+                if valid_insert(temp, board, i, j):
+                    board[i][j] = temp
+                    filled = True
+    print(board)
+
+create_finished_puzzle(6)
